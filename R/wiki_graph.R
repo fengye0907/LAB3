fd438dcbc5d9de1237619551f256e1588cc336d1
Zijie Feng <fengye0907@outlook.com>
2018-09-14 16:39:05 +0200

    data

---
+++ R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' wiki_graph
+#'
+#' @description wiki_graph data is for testing dijstra method.
+#'
+#' @docType data
+#'
+#' @format data.farme
+#'
+#' @keywords datasets
+#'
+#' @references \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+#'
+"wiki_graph"
