b4167d6f6cb2c73eb3e645a8d2b3d3b9eb7a89fb
muh_daud_se <muhammad12daud@gmail.com>
2018-09-14 15:32:25 +0200

    Daud
    Cheak the new comment

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,5 +1,7 @@
 #' Dijkstras algorithm takes a graph and an initial node and calculates the shortest path from the
 #' initial node to every other node in the graph.
+#' 
+#' Daudd
 #'
 #' @param graph A data.frame with three variables (v1, v2 and w) that contains the edges of the graph (from v1 to v2) with the weight of the edge (w).
 #' @param init_node A numeric scalar that exist in the graph.
